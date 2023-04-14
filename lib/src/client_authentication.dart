941cfd46f5b7b93f3c1e6c87d1874a50f8301bf3
Baxi19 <randald1991@gmail.com>
2023-04-14 15:16:36 -0600

    feat (refresh class): Refresh method

--- lib/src/client_authentication.dart
+++ lib/src/client_authentication.dart
@@ -7,29 +7,29 @@ class ClientAuthentication {
   final PkcePair pkcePair;
   ClientAuthentication({required this.pkcePair});
 
-  // static Future<TokenResponseDataModel?> regenerateAccessToken({
-  //   required String refreshToken,
-  //   required String tenant,
-  //   required String policy,
-  //   required String clientId,
-  // }) async {
-  //   var url = "https://$tenant.b2clogin.com/$tenant.onmicrosoft.com/$policy/oauth2/v2.0/token";
-  //   Response response = await Dio().post(
-  //     url,
-  //     data: {
-  //       'grant_type': Constants.refreshToken,
-  //       'scope': Constants.defaultScopes,
-  //       'client_id': clientId,
-  //       'refresh_token': refreshToken,
-  //     },
-  //     options: Options(contentType: Headers.formUrlEncodedContentType),
-  //   );
-  //   if (response.statusCode == 200) {
-  //     return TokenResponseDataModel.fromJson(response.data);
-  //   } else {
-  //     return null;
-  //   }
-  // }
+  static Future<TokenResponseDataModel?> regenerateAccessToken({
+    required String refreshToken,
+    required String tenant,
+    required String policy,
+    required String clientId,
+  }) async {
+    var url = "https://$tenant.b2clogin.com/$tenant.onmicrosoft.com/$policy/oauth2/v2.0/token";
+    Response response = await Dio().post(
+      url,
+      data: {
+        'grant_type': Constants.refreshToken,
+        'scope': Constants.defaultScopes,
+        'client_id': clientId,
+        'refresh_token': refreshToken,
+      },
+      options: Options(contentType: Headers.formUrlEncodedContentType),
+    );
+    if (response.statusCode == 200) {
+      return TokenResponseDataModel.fromJson(response.data);
+    } else {
+      return null;
+    }
+  }
 
   Future<TokenResponseDataModel?> getAllTokens({
     required String redirectUri,
