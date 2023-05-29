cf4d99950df1f7b3010d2d884e30696b23faedaa
Baxi19 <randald1991@gmail.com>
2023-05-28 20:46:26 -0600

    feature (optional params): Export of class necesary for Optional Params flow

--- lib/src/src.dart
+++ lib/src/src.dart
@@ -4,3 +4,4 @@ export 'presentation/aad_b2c_webview.dart';
 export 'services/client_authentication.dart';
 export 'constants.dart';
 export 'services/models/token.dart';
+export 'services/models/optional_param.dart';
